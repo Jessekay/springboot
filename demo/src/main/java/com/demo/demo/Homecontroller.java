2ea491219b768988c1d3e289aa3c00dee9810fd5
JesseKay <kayjes00@gmail.com>
2025-09-29 12:30:10 +0200

    changed the app name to store

--- demo/src/main/java/com/demo/demo/Homecontroller.java
+++ demo/src/main/java/com/demo/demo/Homecontroller.java
@@ -1,19 +1,17 @@
 package com.demo.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Homecontroller {
 
-    @RequestMapping("/")
-    public String index() {
-        String viewName = getViewName();
-        System.out.println(viewName);
-        return viewName;
-    }
+    @Value("${spring.application.name}")
+    private String appName;
 
-    private String getViewName() {
-        return "index.html";
-    }
+    @RequestMapping("/")
+    public String index() { 
+        System.out.println("appName: " + appName);
+        return "index.html"; }
 }
