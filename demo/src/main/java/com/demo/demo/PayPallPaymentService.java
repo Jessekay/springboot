cf2e50bd2285f95aca9637726427048fca6764dd
Jessekay <kayjes00@gmail.com>
2025-10-02 09:43:03 +0200

    class should be opened for extension but not for modification

--- demo/src/main/java/com/demo/demo/PayPallPaymentService.java
+++ demo/src/main/java/com/demo/demo/PayPallPaymentService.java
@@ -1,11 +1,9 @@
 package com.demo.demo;
 
-public class PayPallPaymentService implements PaymentService{
-
+public class PayPallPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
         System.out.println("PayPall");
         System.out.println("Amount: " + amount);
     }
-
 }
